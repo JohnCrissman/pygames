910916ede64f7eb878bc7d01c60a1cec838c8501
John Crissman <JohnMCrissmanUSMC@gmail.com>
2020-06-09 19:01:53 -0500

    starting shmup

--- intro/pygame_template.py
+++ intro/pygame_template.py
@@ -21,17 +21,28 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("My Game")
 clock = pygame.time.Clock()
 
+all_sprites = pygame.sprite.Group()
+
+
 # Game loop
 running = True
 while running:
+    # keep loop running at the right speed
+    clock.tick(FPS)
+
     # Process input (events)
     for event in pygame.event.get():
         # check for closing window
         if event.type == pygame.QUIT:
             running = False
+
     # Update
+    all_sprites.update()
+
+
     # Draw / render
     screen.fill(BLACK)
+    all_sprites.draw(screen)
     # after* drawing everything, flip the display
     # "do the flip last"
     pygame.display.flip()
